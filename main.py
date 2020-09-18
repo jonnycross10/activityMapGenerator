d9185a24b0b2750d58e6315ff490fa15e665ab58
jonnycross10 <crossjonny5@gmail.com>
2020-09-18 11:53:31 -0700

    Get's my activity map and gives it a theme depending on the weekday, and updates the png

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from cairosvg import svg2png
-from svgFile import svg
+from datetime import date
 
+#get today's date as a number
+d = date.today()
+today = d.weekday()
 
-img = "https://ghchart.rshah.org/jonnycross10.svg"
+img = "https://ghchart.rshah.org/"
+color = ["ffff00","ffc0cb","008000","ffa500","409ba5","800080","ff0000"]
 
+#assign the color of the day to the variable
+todaysColor = color[today]
 
-svg2png(url= img, write_to='output.png')
+#assemble the url string with the color and my github id
+urlString = "https://ghchart.rshah.org/" + todaysColor +"/jonnycross10"
 
-
-
+#update the map picture
+svg2png(url= urlString, write_to='map.png')
